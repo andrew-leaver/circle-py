12d8f7b5888486a76e3046cd984fed327fd70b97
Andrew <leaver.andrew@gmail.com>
2016-03-12 17:13:35 -0800

    Added a for loop

--- circle.py
+++ circle.py
@@ -1,12 +1,16 @@
 from graphics import *
+from time import sleep
 def main():
-    win = GraphWin("My Circle", 100, 100)
+    win = GraphWin("My Circle", 200, 200)
     c = Circle(Point(50,50), 10)
     c.setFill("red")
     c.draw(win)
     win.getMouse() # pause for click in window
     c.move(10,10)
     win.getMouse() # pause one more time
+    for num in range (1, 30) :
+        c.move (5,2.5)
+        sleep(0.05)
     win.close()
 
 main()
