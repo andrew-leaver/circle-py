429d8dbd9d2484808ca733bc836d39ee83e0e51d
rustycoder <andrew-leaver@users.noreply.github.com>
2024-04-19 09:30:29 -0700

    feature(circle): Add title

--- circle.py
+++ circle.py
@@ -6,12 +6,16 @@ def main():
     win = GraphWin("My Circle", 200, 200)
     c = Circle(Point(50,50), 10)
     c.setFill("red")
+    message = Text(Point(70,10), "Click to start move!")
     c.draw(win)
-    win.getMouse() # pause for click in window
-    c.move(10,10)
+    message.draw(win)
+    sleep(1)
     win.getMouse() # pause one more time
     for num in range (1, 30) :
         c.move (5,2.5)
+        sleep(0.05)
+    for num in range (1, 30) :
+        c.move(-5, 2.5)
         sleep(0.05)
     win.close()
 
